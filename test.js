a555c4637389871773f08e4a96d02b369ce0d8a6
Gabriel <gabrielj.harel@gmail.com>
2018-10-15 11:51:28 -0400

    add example files

---
+++ test.js
@@ -0,0 +1,5 @@
+const add = require(".");
+
+test("test1", () => {
+    expect(add(1, 1)).toBe(2);
+});
---
+++ test/test.js
@@ -0,0 +1,5 @@
+const add = require("..");
+
+test("test2", () => {
+    expect(add(1, 1)).toBe(2);
+});
